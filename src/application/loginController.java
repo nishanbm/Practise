a00ab4329f0f7bd1c625ee4e0e4b66e3710a880a
burja <nishan8magar@gmail.com>
2016-06-23 22:47:23 -0700

    First commit

---
+++ src/application/loginController.java
@@ -0,0 +1,70 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class loginController {
+	@FXML
+	private Label lblMessage;
+	@FXML
+	private TextField txtUsername;
+	@FXML
+	private TextField txtPassword;
+	
+	public void function (ActionEvent event) throws Exception
+	{
+		if(txtUsername.getText().equals("") && txtPassword.getText().equals(""))
+		{
+			lblMessage.setText("Enter Username and Password");
+		}
+		else if(txtUsername.getText().equals(""))
+		{
+			lblMessage.setText("Enter Username");
+		}
+		else if(txtPassword.getText().equals(""))
+		{
+			lblMessage.setText("Enter Password");
+		}
+		else
+		{
+			if(txtUsername.getText().equals("admin") && txtPassword.getText().equals("admin"))
+			{
+				lblMessage.setText("LOGIN SUCCESSFUL");
+				Stage primaryStage = new Stage();
+				Parent root = FXMLLoader.load(getClass().getResource("/application/mainPage.fxml"));
+				Scene scene = new Scene(root,400,400);
+				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+				txtUsername.setText("");
+				txtPassword.setText("");
+				primaryStage.setScene(scene);
+				primaryStage.show();
+			}
+			else
+			{
+				lblMessage.setText("LOGIN DENIED");
+				txtPassword.setText("");
+			}
+		}
+	}
+	public void cancelfunction (ActionEvent event)
+	{
+		txtUsername.setText("");
+		txtPassword.setText("");
+	}
+	public void addMedicine (ActionEvent event) throws Exception
+	{
+		Stage primaryStage = new Stage();
+		Parent root = FXMLLoader.load(getClass().getResource("/application/addMedicine.fxml"));
+		Scene scene = new Scene(root,400,400);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+	}
+
+}
